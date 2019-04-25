f4ac239c72b90ac13c0f30e87c23ab89bc3f17ad
moving-line <movinglinecheck@gmail.com>
2019-04-26 04:27:05 +0900

    Add temp URL mapping
    - 미구현 기능 관련 URL을 임시로 직접 html file에 연결

---
+++ src/main/java/codesquad/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package codesquad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        //아직 구현 못한 URL을 html 파일에 임시매핑. 구현시 삭제할 것.
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("/questions/form").setViewName("qna/form");
+    }
+}
