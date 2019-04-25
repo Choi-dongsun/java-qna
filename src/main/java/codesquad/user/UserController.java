6fa00b559d832d4bda720795c6d5bdf357f2899c
moving-line <movinglinecheck@gmail.com>
2019-04-26 03:22:44 +0900

    Add User join func
    - 회원가입 기능 구현
    - 회원관련 html file 경로 변경

---
+++ src/main/java/codesquad/user/UserController.java
@@ -0,0 +1,27 @@
+package codesquad.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("users")
+public class UserController {
+    List<User> users = new ArrayList<>();
+
+    @GetMapping("/form")
+    public String form() {
+        return "user/form";
+    }
+
+    @PostMapping("/create")
+    public String create(User user) {
+        users.add(user);
+        return "user/list";
+    }
+}
