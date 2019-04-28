cb6d66aca14e3e6e714b819568d5f9dcf966be74
moving-line <movinglinecheck@gmail.com>
2019-04-28 20:22:12 +0900

    Add Question list func
    - 질문목록 기능 구현

--- src/main/java/codesquad/qna/QnaController.java
+++ src/main/java/codesquad/qna/QnaController.java
@@ -1,12 +1,12 @@
 package codesquad.qna;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -22,7 +22,12 @@ public class QnaController {
     @PostMapping("")
     public String create(Question question) {
         questions.add(question);
-        System.out.println(Arrays.toString(questions.toArray()));
         return "redirect:/";
+    }
+
+    @GetMapping("")
+    public String list(Model model) {
+        model.addAttribute("questions", questions);
+        return "/index";
     }
 }
