a96b58be95988ef8b9e22275ec4b42795cfb4bc6
Dongsun Choi <movinglinecheck@gmail.com>
2019-04-28 22:37:09 +0900

    Step1. DB없이 기능구현 (#1)
    * Add User join func
       - 회원가입 기능 구현
       - 회원관련 html file 경로 변경
    
    * Add User list func
       - 회원목록 기능 구현
    
    * Add User profile func
       - 회원 정보보기 기능 구현
    
    * Refactor duplicate template
       - html file 중복 제거
       - 구현된 기능에 한해, 올바른 mapping 적용
    
    * Add temp URL mapping
       - 미구현 기능 관련 URL을 임시로 직접 html file에 연결
    
    * Add Question create func
       - 질문하기 기능 구현
    
    * Add Question list func
       - 질문목록 기능 구현
    
    * Add Question show func
       - 질문 상세보기 기능 구현
    
    * Modify URL mapping
       - 구현된 기능에 한해, 올바른 mapping 적용
       - URL convention 적용(UserController의 create method URL 변경)
       - 불필요한 annotation 제거
    
    * Add User update func
       - 회원정보 수정 기능 구현

---
+++ src/main/java/codesquad/qna/Question.java
@@ -0,0 +1,50 @@
+package codesquad.qna;
+
+public class Question {
+    private long id;
+    private String writer;
+    private String title;
+    private String contents;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id='" + id + '\'' +
+                ", writer='" + writer + '\'' +
+                ", title='" + title + '\'' +
+                ", contents='" + contents + '\'' +
+                '}';
+    }
+}
